40a60eecf6d408908334bc35c270fc23b63a986c
AaryaCoder <84276676+AaryaCoder@users.noreply.github.com>
2022-02-08 19:03:32 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -15,7 +15,50 @@ function take_snapshot(){
 }
 
 console.log("ml5version:"+ml5.version);
-classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/7rP-Nput2/model.json',modelLoaded);
+classifier=ml5.imageClassifier('https://storage.googleapis.com/tm-model/7rP-Nput2/model.json',modelLoaded);
 function modelLoaded(){
     console.log("Model Loaded");
 }
+
+function speak(){
+    var synth = window.speechSynthesis;
+    speak_data_1 = "The first prediction is " + prediction_1;
+    speak_data_2 = "And the second prediction is " + prediction_2;
+    var utterThis = new SpeechSynthesisUtterance(speak_data_1 + speak_data_2);
+    synth.speak(utterThis);
+  }
+  
+  
+    function check()
+    {
+      img = document.getElementById('captured_image');
+      classifier.classify(img, gotResult);
+    }
+  
+  
+  function gotResult(error, results) {
+    if (error) {
+      console.error(error);
+    } else {
+      console.log(results);
+      document.getElementById("result_emotion_name").innerHTML = results[0].label;
+      prediction = results[0].label;
+      speak();
+      if(results[0].label == "amazing")
+      {
+          document.getElementById("update_emoji").innerHTML = "&#128076;";
+          <h4> Amazing job! </h4>
+      }
+      if(results[0].label == "great")
+      {
+          document.getElementById("update_emoji").innerHTML = "&#128077;";
+          <h4> This is great!</h4>
+      }
+      if(results[0].label == "peace")
+      {
+          document.getElementById("update_emoji").innerHTML = "&#9996;";
+          <h4> Peace out, you are epic!</h4>
+      }
+    }
+  }
+  
